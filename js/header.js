5c0b6b097162bf1dc805312f2449bf23be86decb
Jiadong Chen <jiadong.chen1@student.kpu.ca>
2023-11-14 23:48:44 -0800

    Fix issue due to previous merge

--- js/header.js
+++ js/header.js
@@ -2,8 +2,8 @@ const searchInput = document.querySelector('.header-search-input');
 
 searchInput.addEventListener('keyup', function (e) {
   if (e.code === 'Enter') {
-    let keywords = searchInput.value.trim();
+      let keywords = searchInput.value.trim();
 
-    window.location.href = `list.php?search=${keywords}`;
-  }
-});
+      window.location.href = `list.php?search=${keywords}`;
+    }
+  });
